06d67d6ea925082638e99b333b40bab29a3abe11
sricharaneluri <charansri21@gmail.com>
2023-01-30 08:59:03 -0500

    intial commit

---
+++ src/main/java/com/tech/internal/GoogleCloudTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tech.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudTestServiceApplication.class, args);
+	}
+
+}
